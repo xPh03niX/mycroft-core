c366be3442dd513fb074c90b2f8a1b9e80f626dc
David Wagner <david@wagnerkc.com>
2019-09-18 00:45:22 +0000

    If paired start wifi setup

--- mycroft/client/enclosure/mark2pi/__init__.py
+++ mycroft/client/enclosure/mark2pi/__init__.py
@@ -92,28 +92,22 @@ class EnclosureMark2Pi(Enclosure):
         # TODO: This should live in the derived Enclosure, e.g. EnclosureMark1
         LOG.info("Checking internet connection")
         if not connected():  # and self.conn_monitor is None:
-            if has_been_paired():
-                # TODO: Enclosure/localization
-                self.speak("This unit is not connected to the Internet. "
-                           "Either plug in a network cable or setup your "
-                           "wifi connection.")
-            else:
-                # Begin the unit startup process, this is the first time it
-                # is being run with factory defaults.
+            # Begin the unit startup process, this is the first time it
+            # is being run with factory defaults.
 
-                # TODO: This logic should be in EnclosureMark1
-                # TODO: Enclosure/localization
+            # TODO: This logic should be in EnclosureMark1
+            # TODO: Enclosure/localization
 
-                # Don't listen to mic during this out-of-box experience
-                self.bus.emit(Message("mycroft.mic.mute"))
-                # Setup handler to unmute mic at the end of on boarding
-                # i.e. after pairing is complete
-                self.bus.once('mycroft.paired', self._handle_pairing_complete)
+            # Don't listen to mic during this out-of-box experience
+            self.bus.emit(Message("mycroft.mic.mute"))
+            # Setup handler to unmute mic at the end of on boarding
+            # i.e. after pairing is complete
+            self.bus.once('mycroft.paired', self._handle_pairing_complete)
 
-                self.speak(mycroft.dialog.get('mycroft.intro'))
-                wait_while_speaking()
-                time.sleep(2)  # a pause sounds better than just jumping in
+            self.speak(mycroft.dialog.get('mycroft.intro'))
+            wait_while_speaking()
+            time.sleep(2)  # a pause sounds better than just jumping in
 
-                # Kick off wifi-setup automatically
-                data = {'allow_timeout': False, 'lang': self.lang}
-                self.bus.emit(Message('system.wifi.setup', data))
+            # Kick off wifi-setup automatically
+            data = {'allow_timeout': False, 'lang': self.lang}
+            self.bus.emit(Message('system.wifi.setup', data))
